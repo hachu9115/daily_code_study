32ccfdf5f43537109a1b302d63a80cd97a00b51e
jongseo <jslee098@gmail.com>
2025-08-30 23:08:34 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 17.86 ms, Memory: 7.45 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,15 +1,14 @@
+str_input = str(input())
 
-def swap_case(str):
-    result = []
-    for i in str:
-        if i.islower():
-            result.append(i.upper())
-        elif i.isupper():
-            result.append(i.lower())
+def switch_bs(text):
+    answer = []
+    for i in text:
+        if i == i.upper():
+            answer.append(i.lower())
+        elif i != i.upper():
+            answer.append(i.upper())
         else:
-            result.append(i)
-    return ''.join(result)
+            print("영어가 아닌 문자가 삽입되었습니다")
+    return ''.join(answer)
 
-str = input()
-output_str = swap_case(str)
-print(output_str)
+print(switch_bs(str_input))
