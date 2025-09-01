e93fb0301e6954b832bf051d5a3c239df5e4add7
jongseo <jslee098@gmail.com>
2025-09-01 22:18:57 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,7 +1,5 @@
-def solution(number, n, m):
-    if number%n == 0 and number%m == 0:
-        answer = 1
-        
+def solution(number,n,m):
+    if (number%n == 0) & (number%m ==0):
+        return 1
     else:
-        answer = 0
-    return answer
+        return 0
