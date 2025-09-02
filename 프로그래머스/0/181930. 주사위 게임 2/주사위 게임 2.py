a55f22d7e5a7b3edc8b3f80116852273e6006101
jongseo <jslee098@gmail.com>
2025-09-02 15:45:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,16 +1,8 @@
-def solution(a, b, c):
-    # Case 1: 모든 숫자가 다를 때
-    if a != b and b != c and a != c:
-        return a + b + c
+def solution(a,b,c):
+    if a != b != c!=a:
+        return a+b+c
+    elif a==b and a==c and b==c:
+        return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    elif (a==b!=c) or (a!=b==c) or (a==c!=b):
+        return (a+b+c)*(a**2+b**2+c**2)
     
-    # 각 숫자의 제곱과 세제곱 계산
-    sum_of_squares = a**2 + b**2 + c**2
-    sum_of_cubes = a**3 + b**3 + c**3
-    
-    # Case 2: 두 숫자가 같고 하나가 다를 때
-    if (a == b and b != c) or (a == c and b != c) or (b == c and a != b):
-        return (a + b + c) * sum_of_squares
-    
-    # Case 3: 세 숫자가 모두 같을 때
-    if a == b == c:
-        return (a + b + c) * sum_of_squares * sum_of_cubes
