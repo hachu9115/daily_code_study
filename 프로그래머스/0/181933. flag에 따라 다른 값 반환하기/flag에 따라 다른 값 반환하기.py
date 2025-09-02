8381e47c7fb0129a572123b6f036a69970320d95
jongseo <jslee098@gmail.com>
2025-09-02 13:33:20 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.py
@@ -1,6 +1,5 @@
-def solution(a, b, flag):
-    if flag == True:
-        answer = a+b
-    elif flag == False:
-        answer = a-b
-    return answer
+def solution(a,b,flag):
+    if flag:
+        return a+b
+    else:
+        return a-b
