6e1c61eb3361dc40fb4578555e5548cb1e32e69a
jongseo <jslee098@gmail.com>
2025-09-02 14:51:07 +0900

    [level 0] Title: 코드 처리하기, Time: 12.10 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,21 +1,11 @@
 def solution(code):
     mode = 0
     ret = []
-    
     for idx, char in enumerate(code):
-        if mode == 0:
-            if char == '1':
-                mode = 1
-            else:
-                if idx % 2 == 0:
-                    ret.append(char)
-        elif mode == 1:
-            if char == '1':
-                mode = 0
-            else:
-                if idx % 2 != 0:
-                    ret.append(char)
-    
-    ret_string = ''.join(ret)
-    
-    return ret_string if ret_string else "EMPTY"
+        if char == "1":
+            mode = 1 - mode
+        elif idx%2 == mode:
+            ret.append(char)
+    return ''.join(ret) or "EMPTY"
+            
+        
